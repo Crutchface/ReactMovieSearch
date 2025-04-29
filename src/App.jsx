7fb107ae908f439ac9119b81dfe81870cf491657
Chris <chris.dg.ferguson@gmail.com>
2025-04-29 11:37:16 -0600

    Commit React Demo1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './css/App.css';
+import Home from "./pages/Home"
+import {Route, Routes} from "react-router-dom";
+import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
+import {MovieProvider} from "./contexts/MovieContext.jsx";
+
+
+//Components always captalized
+function App() {
+  return (
+      <MovieProvider>
+          <NavBar />
+      <main className="main-content">
+          <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/favorites" element={<Favorites />}/>
+          </Routes>
+      </main>
+      </MovieProvider>
+  )
+}
+
+export default App
